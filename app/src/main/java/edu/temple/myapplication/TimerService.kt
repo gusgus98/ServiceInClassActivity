563b959ed47233ef7b0d150332919e91461d3dc0
gusgus98 <82188302+gusgus98@users.noreply.github.com>
2023-11-09 15:09:23 -0500

    First Part

--- app/src/main/java/edu/temple/myapplication/TimerService.kt
+++ app/src/main/java/edu/temple/myapplication/TimerService.kt
@@ -7,7 +7,7 @@ import android.os.IBinder
 import android.util.Log
 
 @Suppress("ControlFlowWithEmptyBody")
-class TimerService : Service() {
+class  TimerService : Service() {
 
     private var isRunning = false
 
